a5963808de5b786f3013a30ff0982eca69b93313
GeorgeTig <georgecorineltigau@yahoo.com>
2023-10-24 15:58:05 +0300

    Creation of class Peg with atributes

---
+++ TwixtLib/Peg.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <vector>
+#include<memory>
+
+#include "IPiece.h"
+
+
+
+
+class Peg :
+    public IPiece
+{
+private:
+    EColor m_color;
+    bool m_isNorthLinked;
+    bool m_isSouthLinked;
+    std::vector<IPiecePtr> m_neighbours;
+};
+
--- TwixtLib/include/IPiece.h
+++ TwixtLib/include/IPiece.h
@@ -1,11 +1,15 @@
 #pragma once
 
+#include "EColor.h"
+
+using IPiecePtr = std::shared_ptr<class IPiece>;
+
 class IPiece
 {
 public:
 	virtual ~IPiece() = default;
-	virtual EColor GetColor() = 0;
-	virtual bool isNorthLinked() = 0;
-	virtual bool isSouthLinked() = 0;
+	virtual EColor GetColor() const = 0;
+	virtual bool isNorthLinked() const = 0;
+	virtual bool isSouthLinked() const = 0;
 
 };
