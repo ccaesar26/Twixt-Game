a595a007d8f6d7b13d4d660d8bdffc50ca2d0df6
GeorgeTig <georgecorineltigau@yahoo.com>
2023-10-24 15:25:26 +0300

    Modified methods to be pure virtual

--- TwixtLib/include/IPiece.h
+++ TwixtLib/include/IPiece.h
@@ -2,10 +2,10 @@
 
 class IPiece
 {
-	
+public:
 	virtual ~IPiece() = default;
-	EColor GetColor();
-	bool isNorthLinked();
-	bool isSouthLinked();
+	virtual EColor GetColor() = 0;
+	virtual bool isNorthLinked() = 0;
+	virtual bool isSouthLinked() = 0;
 
 };
