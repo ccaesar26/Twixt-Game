a54a0a6337b081b3406b55ba21c99af9437d83cc
Cezar Constăndoiu <cezar.constandoiu@student.unitbv.ro>
2023-10-24 15:59:56 +0300

    Added pure virtual methods to class IBoard

--- TwixtLib/include/IBoard.h
+++ TwixtLib/include/IBoard.h
@@ -8,4 +8,10 @@ class IBoard
 {
 public:
 	static IBoardPtr CreateBoard();
+
+	virtual void PlacePiece(Position pos, EColor color) = 0;
+
+	virtual IPiecePtr at(Position pos) const = 0;
+
+	virtual bool CheckIfWinningPlacement(Position pos, EColor currentPlayer) const = 0;
 };
