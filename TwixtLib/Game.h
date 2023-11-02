aff555938fa5038e296a932dcb3469e00bb481d9
Aparascăi Andreea-Mădălina <aaparascai9@gmail.com>
2023-11-03 01:18:56 +0200

    Expanded Game class with additional methods and implemented functionality.

--- TwixtLib/Game.h
+++ TwixtLib/Game.h
@@ -14,11 +14,21 @@ private:
 	
 	void InitializeGame();
 	void InitializeGamr(const string& config, Ecolor turn = EColor::Black);
+	void SwitchTurn();
 	
 public:
 	void AddListener(IGameListenerPtr listener) const override;
 	EColor GetCurrentPlayer() const override;
 	IPiecePtr GetPiecePtr(const Position& pos) const override;
+	bool IsGameOver() const override;
+	bool IsDraw() const override;
+	bool IsWon() const override;
+	void LoadFromFile(const string& fileName) const override;
+	void PlacePiece(const Position& pos) const override;
+	void RemoveListener(IGameListenerPtr listener) const override;
+	void Reset() override;
+	void Restore(const string& config) const override;
+	void SaveToFile(const string& fileName) const override;
 	
 
 };
