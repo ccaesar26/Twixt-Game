3b03b5bf8e9ed43dd08964e7f38b1f03ec76df48
Cezar Constăndoiu <cezar.constandoiu@student.unitbv.ro>
2023-11-07 13:40:46 +0200

    Implemented OutOfBoundsException class

--- TwixtLib/include/gameException.h
+++ TwixtLib/include/gameException.h
@@ -20,3 +20,18 @@ inline GameException::GameException() : std::logic_error("Unknown error")
 inline GameException::GameException(const std::string& message) : std::logic_error(message)
 {
 }
+
+class OutOfBoundsException : public GameException
+{
+public:
+	OutOfBoundsException();
+	OutOfBoundsException(const std::string& message);
+};
+
+inline OutOfBoundsException::OutOfBoundsException() : GameException("Out of bounds")
+{
+}
+
+inline OutOfBoundsException::OutOfBoundsException(const std::string& message) : GameException(message)
+{
+}
