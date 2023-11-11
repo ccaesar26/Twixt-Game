2da6921b8293c4043fbe72c350c8b800056e3015
GitStroberi <andreipilif@gmail.com>
2023-11-11 17:37:04 +0200

    Implementation of Board constructor

--- TwixtLib/Board.cpp
+++ TwixtLib/Board.cpp
@@ -4,3 +4,15 @@ IBoardPtr IBoard::CreateBoard()
 {
 	return std::make_shared<Board>();
 }
+
+Board::Board(int size) : m_size(size) {
+	m_board.resize(m_size);
+	for (int i = 0; i < m_size; i++)
+	{
+		m_board[i].resize(m_size);
+		for (int j = 0; j < m_size; j++)
+		{
+			m_board[i][j] = nullptr;
+		}
+	}
+}
