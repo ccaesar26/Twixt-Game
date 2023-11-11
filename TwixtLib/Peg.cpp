fb0114320de067a804fe5ef894c2b32b6cfd70f4
Aparascăi Andreea-Mădălina <aaparascai9@gmail.com>
2023-11-11 17:54:09 +0200

    Implemented constructor for Peg

--- TwixtLib/Peg.cpp
+++ TwixtLib/Peg.cpp
@@ -4,3 +4,11 @@ IPiecePtr IPiece::Produce(EColor color)
 {
 	return std::make_shared<Peg>(color);
 }
+
+Peg::Peg(EColor color)
+{
+	m_color = color;
+	m_isNorthLinked = false;
+	m_isSouthLinked = false;
+}
+
