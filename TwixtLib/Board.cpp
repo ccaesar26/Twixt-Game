8df1ca08a61c32ba13bafa270a0e99f77247f84c
Cezar Constăndoiu <cezar.constandoiu@student.unitbv.ro>
2023-11-15 21:27:32 +0200

    Code cleanup Board.cpp

--- TwixtLib/Board.cpp
+++ TwixtLib/Board.cpp
@@ -6,7 +6,8 @@ IBoardPtr IBoard::CreateBoard()
 	return std::make_shared<Board>();
 }
 
-Board::Board(const int size) : m_size(size) {
+Board::Board(const int size) : m_size(size)
+{
 	m_board.resize(m_size);
 	for (int i = 0; i < m_size; i++)
 	{
@@ -18,18 +19,23 @@ Board::Board(const int size) : m_size(size) {
 	}
 }
 
-void Board::PlacePiece(const Position pos, const EColor color) {
-	if (pos.row < 0 || pos.row >= m_size || pos.col < 0 || pos.col >= m_size) {
+void Board::PlacePiece(const Position pos, const EColor color)
+{
+	if (pos.row < 0 || pos.row >= m_size || pos.col < 0 || pos.col >= m_size)
+	{
 		throw GameException("Invalid position");
 	}
-	if (m_board[pos.row][pos.col] != nullptr) {
+	if (m_board[pos.row][pos.col] != nullptr)
+	{
 		throw GameException("Position is already occupied");
 	}
 	m_board[pos.row][pos.col] = IPiece::Produce(color);
 }
 
-IPiecePtr Board::At(const Position pos) const {
-	if (pos.row < 0 || pos.row >= m_size || pos.col < 0 || pos.col >= m_size) {
+IPiecePtr Board::At(const Position pos) const
+{
+	if (pos.row < 0 || pos.row >= m_size || pos.col < 0 || pos.col >= m_size)
+	{
 		throw GameException("Invalid position");
 	}
 	return m_board[pos.row][pos.col];
