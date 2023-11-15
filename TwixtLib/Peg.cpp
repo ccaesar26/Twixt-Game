9e9aa4d0931d18bfea68dac540bb6b6a306486e4
Cezar Constăndoiu <cezar.constandoiu@student.unitbv.ro>
2023-11-15 21:18:47 +0200

    Refactor Peg.cpp

--- TwixtLib/Peg.cpp
+++ TwixtLib/Peg.cpp
@@ -5,19 +5,19 @@ IPiecePtr IPiece::Produce(EColor color)
 	return std::make_shared<Peg>(color);
 }
 
-Peg::Peg(EColor color)
+Peg::Peg(const EColor color)
 {
 	m_color = color;
 	m_isNorthLinked = false;
 	m_isSouthLinked = false;
 }
 
-bool Peg::isNorthLinked() const
+bool Peg::IsNorthLinked() const
 {
 	return m_isNorthLinked;
 }
 
-bool Peg::isSouthLinked() const
+bool Peg::IsSouthLinked() const
 {
 	return m_isSouthLinked;
 }
