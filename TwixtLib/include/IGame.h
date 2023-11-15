cad6264fa84bb952f74ebadbbe32388526b30bd4
Cezar Constăndoiu <cezar.constandoiu@student.unitbv.ro>
2023-11-15 21:17:33 +0200

    Refactor IGame

--- TwixtLib/include/IGame.h
+++ TwixtLib/include/IGame.h
@@ -1,10 +1,12 @@
 #pragma once
 
-#include "Position.h"
 #include "EColor.h"
 #include "IPiece.h"
+#include "IGameListener.h"
 
 #include <memory>
+#include <string>
+
 
 using IGamePtr = std::shared_ptr<class IGame>;
 
@@ -18,14 +20,14 @@ public:
 	virtual void Reset() = 0;
 	virtual void Restore(const std::string& config) const = 0;
 	virtual void PlacePiece(const Position& pos) = 0;
-	virtual EColor GetCurrentPlayer() const = 0;
-	virtual IPiecePtr GetPiecePtr(const Position& pos) const = 0;
-	virtual bool IsGameOver() const = 0;
-	virtual bool IsDraw() const = 0;
-	virtual bool IsWon() const = 0;
+	[[nodiscard]] virtual EColor GetCurrentPlayer() const = 0;
+	[[nodiscard]] virtual IPiecePtr GetPiecePtr(const Position& pos) const = 0;
+	[[nodiscard]] virtual bool IsGameOver() const = 0;
+	[[nodiscard]] virtual bool IsDraw() const = 0;
+	[[nodiscard]] virtual bool IsWon() const = 0;
 	virtual void LoadFromFile(const std::string& fileName) const = 0;
 	virtual void SaveToFile(const std::string& fileName) const = 0;
 	virtual void NotifyPiecePlaced(const Position& pos) const = 0;
 	virtual void NotifyGameOver(EGameResult gameResult) const = 0;
 	virtual void NotifyGameRestarted() const = 0;
-}
+};
