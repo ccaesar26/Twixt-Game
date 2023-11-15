a676fe6e7d7dff1f442c15a698323ef3356d59ec
Cezar Constăndoiu <cezar.constandoiu@student.unitbv.ro>
2023-11-15 21:17:18 +0200

    Refactor IBoard

--- TwixtLib/include/IBoard.h
+++ TwixtLib/include/IBoard.h
@@ -10,11 +10,13 @@ using IBoardPtr = std::shared_ptr<class IBoard>;
 class IBoard
 {
 public:
+	virtual ~IBoard() = default;
+
 	static IBoardPtr CreateBoard();
 
 	virtual void PlacePiece(Position pos, EColor color) = 0;
 
-	virtual IPiecePtr At(Position pos) const = 0;
+	[[nodiscard]] virtual IPiecePtr At(Position pos) const = 0;
 
-	virtual bool CheckIfWinningPlacement(Position pos, EColor currentPlayer) const = 0;
+	[[nodiscard]] virtual bool CheckIfWinningPlacement(Position pos, EColor currentPlayer) const = 0;
 };
