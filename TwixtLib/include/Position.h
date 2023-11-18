36b7403657660abc6eebfe32a9e2ca80d02c7558
Aparascăi Andreea-Mădălina <aaparascai9@gmail.com>
2023-11-18 15:50:58 +0200

    Made declarations for necessary methods in Position class

--- TwixtLib/include/Position.h
+++ TwixtLib/include/Position.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <utility>
 
 class Position
 {
@@ -6,9 +7,27 @@ public:
 	int row;
 	int col;
 
+	//Default constructor
 	explicit Position(int r = 0, int c = 0);
-	bool operator==(const Position& pos) const;
-	bool operator!=(const Position& pos) const;
+	
+	//Conversion constructor from pair
+	Position(const std::pair<int, int>& p);
+	
+	//Assignment operator from pair
+	Position& operator=(const std::pair<int, int>& p);
+	
+	//Conversion operator to pair
+	operator std::pair<int, int>() const;
+	
+	//Comparison operators
+	bool operator==(const Position& other) const;
+	bool operator!=(const Position& other) const;
+
+
 };
+
+//Comparison operators for comparing Position with std::pair<int, int>
+bool operator==(const std::pair<int, int>& p, const Position& pos);
+bool operator!=(const std::pair<int, int>& p, const Position& pos);
 
 // TODO: 'inline' implementation of Position's methods
