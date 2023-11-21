f15d4d8f1ebd53d53a8f41fe7a84523b1ff2f4e5
Cezar Constăndoiu <cezar.constandoiu@student.unitbv.ro>
2023-11-21 13:12:37 +0200

    inheritance of IGameListener in Qt

--- TwixtGUIQt/TwixtGUIQt.h
+++ TwixtGUIQt/TwixtGUIQt.h
@@ -2,14 +2,19 @@
 
 #include <QtWidgets/QMainWindow>
 #include "ui_TwixtGUIQt.h"
+#include "IGameListener.h"
 
-class TwixtGUIQt : public QMainWindow
+class TwixtGUIQt : public QMainWindow, public IGameListener
 {
     Q_OBJECT
 
 public:
     TwixtGUIQt(QWidget *parent = nullptr);
     ~TwixtGUIQt();
+
+    void OnPiecePlaced(const Position& pos) override;
+    void OnGameOver(const EGameResult& result) override;
+    void OnGameRestarted() override;
 
 private:
     Ui::TwixtGUIQtClass ui;
