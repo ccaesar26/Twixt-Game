e111a9ab994444dfb69d5eeeaa6e5ba270024c5b
Cezar Constăndoiu <cezar.constandoiu@student.unitbv.ro>
2023-11-21 13:19:11 +0200

    Update main.cpp

--- TwixtGUIQt/main.cpp
+++ TwixtGUIQt/main.cpp
@@ -4,7 +4,14 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    TwixtGUIQt w;
-    w.show();
+
+    std::shared_ptr<TwixtGUIQt> w = std::make_shared<TwixtGUIQt>();
+
+    std::shared_ptr<IGame> gameLogic = IGame::CreateGame();
+
+    gameLogic->AddListener(w);
+
+    w->show();
+
     return a.exec();
 }
