3041837028e8f52d9c5da8d3e2060971b074b3e2
Cezar Constăndoiu <cezar.constandoiu@student.unitbv.ro>
2023-11-21 10:12:52 +0200

    Update Position.h

--- TwixtLib/include/Position.h
+++ TwixtLib/include/Position.h
@@ -30,4 +30,32 @@ public:
 bool operator==(const std::pair<int, int>& p, const Position& pos);
 bool operator!=(const std::pair<int, int>& p, const Position& pos);
 
-// TODO: 'inline' implementation of Position's methods
+
+inline Position::Position(int r, int c) : row(r), col(c) {}
+inline Position::Position(const std::pair<int, int>& p) : row(p.first), col(p.second) {}
+inline Position& Position::operator=(const std::pair<int, int>& p)
+{
+	row = p.first;
+	col = p.second;
+	return *this;
+}
+inline Position::operator std::pair<int, int>() const
+{
+	return std::make_pair(row, col);
+}
+inline bool Position::operator==(const Position& other) const
+{
+	return row == other.row && col == other.col;
+}
+inline bool Position::operator!=(const Position& other) const
+{
+	return !(*this == other);
+}
+inline bool operator==(const std::pair<int, int>& p, const Position& pos)
+{
+	return p.first == pos.row && p.second == pos.col;
+}
+inline bool operator!=(const std::pair<int, int>& p, const Position& pos)
+{
+	return !(p == pos);
+}
