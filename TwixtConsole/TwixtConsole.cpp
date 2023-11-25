f18a33079e573c29b62b5124809caf1b93c024e9
GeorgeTig <georgecorineltigau@yahoo.com>
2023-11-25 20:29:30 +0200

    Added m_game member and setter

--- TwixtConsole/TwixtConsole.cpp
+++ TwixtConsole/TwixtConsole.cpp
@@ -3,3 +3,8 @@
 TwixtConsole::TwixtConsole()
 {
 }
+
+void TwixtConsole::setGame(IGamePtr game)
+{
+	m_game = std::move(game);
+}
--- TwixtConsole/TwixtConsole.h
+++ TwixtConsole/TwixtConsole.h
@@ -1,10 +1,17 @@
 #pragma once
+#include<iostream>
+#include <utility>
+#include "IGame.h"
 #include "IGameListener.h"
 
 class TwixtConsole : public IGameListener
 {
 public:
 	TwixtConsole();
+	void setGame(IGamePtr game);
+private:
+	IGamePtr m_game;
+	
 
 
 };
