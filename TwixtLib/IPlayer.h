c42276d7b6cdcde464480e097ef55c85409ee2b5
GitStroberi <andreipilif@gmail.com>
2023-11-29 20:49:55 +0200

    Added member variables and methods to the Player interface

--- TwixtLib/IPlayer.h
+++ TwixtLib/IPlayer.h
@@ -1,13 +1,25 @@
 #pragma once
-
+#include "IPiece.h"
 #include <memory>
+#include <vector>
+#include <string>
 
 using IPlayerPtr = std::shared_ptr<class IPlayer>;
+using Links = std::pair<IPiecePtr, IPiecePtr>;
 
 class IPlayer
 {
+private:
+	std::vector<IPiecePtr> m_pegs;
+	std::vector<Links> m_links;
+	EColor m_color;
+	std::string m_name;
+	
 public:
 	virtual ~IPlayer() = default;
 	static IPlayerPtr CreatePlayer();
-	
+	[[nodiscard]] virtual std::vector<IPiecePtr> GetPegs() const = 0;
+	[[nodiscard]] virtual std::vector<Links> GetLinks() const = 0;
+	[[nodiscard]] virtual EColor GetColor() const = 0;
+	[[nodiscard]] virtual std::string GetName() const = 0;
 };
