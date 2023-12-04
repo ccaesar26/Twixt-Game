51688325b4d0f91dc0c8564d3b3ee7a7b5b3ad32
Cezar Constăndoiu <cezar.constandoiu@student.unitbv.ro>
2023-12-04 14:24:41 +0200

    Added HoleButton constructor (without color)

--- TwixtGUIQt/HoleButton.cpp
+++ TwixtGUIQt/HoleButton.cpp
@@ -1,1 +1,8 @@
 #include "HoleButton.h"
+
+HoleButton::HoleButton(const Position& position, QWidget* parent)
+	: QPushButton(parent)
+	, m_position(position)
+	, m_color(std::nullopt)
+{
+}
--- TwixtGUIQt/HoleButton.h
+++ TwixtGUIQt/HoleButton.h
@@ -1,13 +1,21 @@
 #pragma once
 #include <qpushbutton.h>
+
+#include "EColor.h"
+#include "Position.h"
+
 class HoleButton :
     public QPushButton
 {
     Q_OBJECT
 public:
+    HoleButton(const Position& position, QWidget* parent = nullptr);
+
 
 signals:
 
 private:
+    Position m_position;
+    std::optional<EColor> m_color;
 };
 
