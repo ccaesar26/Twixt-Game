ec4da38769110169bd6cf3754aaeece2e4dc1726
GitStroberi <andreipilif@gmail.com>
2023-12-10 19:47:58 +0200

    Property and syntax fixing

--- TwixtConsole/Source.cpp
+++ TwixtConsole/Source.cpp
@@ -1,7 +1,6 @@
 #include "TwixtConsole.h"
 #include<utility>
 
-
 int main()
 {
 	std::shared_ptr console = std::make_shared<TwixtConsole>();
@@ -14,49 +13,50 @@ int main()
 	int x, y;
 	int x2, y2, x1, y1;
 	Position pos1, pos2;
-	while(n!= 9)
+	while (n != 9)
 	{
-	switch (n)
-	{
-	case 0:
-		std::cout << "Introduceti pozitia in care doriti sa introduceti piesa:\n";
-		std::cin >> x >> y;
-		pos1 = Position(x, y);
-		console->PlacePiece(pos1);
-		break;
-	case 1:
-		std::cout << "Introduceti poziitiile pieselor intre care doriti sa faceti pod:\n";
-		std::cout << "Introduceti pozitiilei primei piese: ";
-		std::cin >> x1 >> y1;
-		pos1 = Position(x1, y1);
-		std::cout << "\n";
-		std::cout << "Introduceti pozitiilei piesei a2-a: ";
-		std::cin >> x2 >> y2;
-		pos2 = Position(x2, y2);
-		console->PlaceBridge(pos1,pos2);
-		break;
-	case 2:
-		std::cout << "Introduceti poziitiile pieselor intre care doriti sa stergeti pod-ul:\n";
-		std::cout << "Introduceti pozitiilei primei piese: ";
-		std::cin >> x1 >> y1;
-		pos1 = Position(x1, y1);
-		std::cout << "\n";
-		std::cout << "Introduceti pozitiilei piesei a2-a: ";
-		std::cin >> x2 >> y2;
-		pos2 = Position(x2, y2);
-		console->DeleteBridge(pos1, pos2);
-		break;
-	case 3:
-		break;
-	case 4:
-		break;
-	case 5:
-		break;
-	case 6:
-		console->EndTurn();
-		console->printHelperMenu();
-		std::cin >> n;
-		break;
+		switch (n)
+		{
+		case 0:
+			std::cout << "Introduceti pozitia in care doriti sa introduceti piesa:\n";
+			std::cin >> x >> y;
+			pos1 = Position(x, y);
+			console->PlacePiece(pos1);
+			break;
+		case 1:
+			std::cout << "Introduceti poziitiile pieselor intre care doriti sa faceti pod:\n";
+			std::cout << "Introduceti pozitiilei primei piese: ";
+			std::cin >> x1 >> y1;
+			pos1 = Position(x1, y1);
+			std::cout << "\n";
+			std::cout << "Introduceti pozitiilei piesei a2-a: ";
+			std::cin >> x2 >> y2;
+			pos2 = Position(x2, y2);
+			console->PlaceBridge(pos1, pos2);
+			break;
+		case 2:
+			std::cout << "Introduceti poziitiile pieselor intre care doriti sa stergeti pod-ul:\n";
+			std::cout << "Introduceti pozitiilei primei piese: ";
+			std::cin >> x1 >> y1;
+			pos1 = Position(x1, y1);
+			std::cout << "\n";
+			std::cout << "Introduceti pozitiilei piesei a2-a: ";
+			std::cin >> x2 >> y2;
+			pos2 = Position(x2, y2);
+			console->DeleteBridge(pos1, pos2);
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			console->EndTurn();
+			console->printHelperMenu();
+			std::cin >> n;
+			break;
+		}
+		return 0;
 	}
-	return 0;
 }
