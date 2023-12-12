a2111c53053083e277e010a32158c82d4f82a11d
Cezar Constăndoiu <93541398+ccaesar26@users.noreply.github.com>
2023-12-12 16:46:02 +0200

    Partial implementation of TwixtGUIQt constructor

--- TwixtGUIQt/TwixtGUIQt.cpp
+++ TwixtGUIQt/TwixtGUIQt.cpp
@@ -1,12 +1,22 @@
 #include "TwixtGUIQt.h"
 
+#include <QGridLayout>
+
 TwixtGUIQt::TwixtGUIQt(QWidget *parent)
     : QMainWindow(parent)
-{    
+{
+    auto mainWidget = new QWidget{};
+    auto mainGridLayout = new QGridLayout{};
+
+    //TODO: initialize components of the main grid
+
+    mainWidget->setLayout(mainGridLayout);
+    this->setCentralWidget(mainWidget);
 }
 
 TwixtGUIQt::~TwixtGUIQt()
-{}
+{
+}
 
 void TwixtGUIQt::OnPiecePlaced(const Position& pos)
 {
