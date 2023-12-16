70b43d971700e4151dadcbb5db82676b0f27d47a
GitStroberi <andreipilif@gmail.com>
2023-12-16 20:41:23 +0200

    Updated peg pieces to have the GetNeighbors and GetPosition methods

--- TwixtLib/Peg.h
+++ TwixtLib/Peg.h
@@ -4,19 +4,23 @@
 #include <memory>
 
 #include "IPiece.h"
+#include <Position.h>
 
 class Peg final :
 	public IPiece
 {
 private:
 	EColor m_color;
+	Position m_position;
 	bool m_isNorthLinked;
 	bool m_isSouthLinked;
 	std::vector<IPiecePtr> m_neighbors;
 
 public:
-	explicit Peg(EColor color);
+	explicit Peg(EColor color, Position pos);
 	[[nodiscard]] bool IsNorthLinked() const override;
 	[[nodiscard]] bool IsSouthLinked() const override;
 	[[nodiscard]] EColor GetColor() const override;
+	std::vector<IPiecePtr> GetNeighbors() const;
+	Position GetPosition() const;
 };
--- TwixtLib/include/IPiece.h
+++ TwixtLib/include/IPiece.h
@@ -9,7 +9,7 @@ using IPiecePtr = std::shared_ptr<class IPiece>;
 class IPiece
 {
 public:
-	static IPiecePtr Produce(EColor color);
+	static IPiecePtr Produce(EColor color, Position pos);
 	virtual ~IPiece() = default;
 	[[nodiscard]] virtual EColor GetColor() const = 0;
 	[[nodiscard]] virtual bool IsNorthLinked() const = 0;
