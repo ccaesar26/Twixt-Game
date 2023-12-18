eb1fa727d4d5cd84938fd7b2d65e7471dbe23d27
GeorgeTig <georgecorineltigau@yahoo.com>
2023-12-18 15:38:50 +0200

    test: PegCreationPosition

--- TwixtTests/PegTest.cpp
+++ TwixtTests/PegTest.cpp
@@ -2,8 +2,15 @@
 
 #include "Peg.h"
 
-TEST(PegCreation, Color)
+TEST(PegCreation, ColorTest)
 {
 	Peg p(EColor::Black,Position (1,1));
 	EXPECT_EQ(p.GetColor(), EColor::Black);
+}
+
+TEST(PegCreation, PositionTest)
+{
+	Peg p(EColor::Black, Position(1, 1));
+	Position pos(1, 1);
+	EXPECT_EQ(p.GetPosition(), pos);
 }
