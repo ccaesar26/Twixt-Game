7e6ae15efe7d71f7e9ef58cd79b184b141645a01
Cezar Constăndoiu <93541398+ccaesar26@users.noreply.github.com>
2023-12-20 17:14:15 +0200

    Changed HoleButton::UpdatePeg() impementation

--- TwixtGUIQt/HoleButton.cpp
+++ TwixtGUIQt/HoleButton.cpp
@@ -55,16 +55,23 @@ void HoleButton::mouseReleaseEvent(QMouseEvent* event)
 
 void HoleButton::UpdatePeg()
 {
+	QString path = "assets/";
 	if (m_color.has_value())
 	{
-		setStyleSheet("background-color: " + ColorToString(m_color.value()) + ";");
+		path += ColorToString(m_color.value());
 	}
 	else
 	{
-		setStyleSheet("background-color: white;");
+		path += "empty";
 	}
-	
-	setStyleSheet("border-radius: 25px;");
+	path += ".png";
+
+	const QPixmap pixmap(path);
+	const QIcon icon(pixmap);
+	setIcon(icon);
+	setIconSize(pixmap.rect().size());
+
+	setStyleSheet("border: none;");
 }
 
 HoleButton::HoleButton(const HoleButton& other)
