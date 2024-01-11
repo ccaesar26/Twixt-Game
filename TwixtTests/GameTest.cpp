361ad6f3dad67a79b4f7d441192490528042ffc6
GeorgeTig <georgecorineltigau@yahoo.com>
2024-01-11 20:39:36 +0200

    Test: GameReset

--- TwixtTests/GameTest.cpp
+++ TwixtTests/GameTest.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 #include "Game.h"
 #include "GameException.h"
+#include "Link.h"
 
 TEST(GameTest, GameCreation)
 {
@@ -30,4 +31,14 @@ TEST(GameTest, GamePiecePlace)
 		std::shared_ptr game = IGame::CreateGame();
 	game->PlacePiece(Position(1, 1));
 	EXPECT_EQ(game->GetPiecePtr(Position(1, 1))->GetColor(), EColor::Red);
+}
+	
+TEST(GameTest, GameReset)
+{
+    std::shared_ptr game = IGame::CreateGame();
+    game->SwitchTurn();
+    std::shared_ptr game1 = IGame::CreateGame();
+    game->Reset();
+    EXPECT_EQ(game->GetCurrentPlayerColor(), game1->GetCurrentPlayerColor());
+
 }
