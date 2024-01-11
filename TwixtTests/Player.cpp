bb8008886138c134b41cd54b14d0b6d68c93cfe7
GeorgeTig <georgecorineltigau@yahoo.com>
2024-01-11 19:18:24 +0200

    Test: RemoveLink

--- TwixtTests/Player.cpp
+++ TwixtTests/Player.cpp
@@ -51,5 +51,20 @@ TEST(PlayerTests, AddLink)
 		player.AddPeg(Position(2, 3));
 		player.AddLink(Position(1, 1), Position(2, 3));
 		EXPECT_EQ(player.GetLinks().size(), 1);
+	    }
+}
+
+TEST(PlayerTests, RemoveLink)
+{
+	IBoardWeakPtr b;
+	if (b.lock())
+	{
+		std::string name = "Vlad";
+		Player player(EColor::Black, name, b);
+		player.AddPeg(Position(1, 1));
+		player.AddPeg(Position(2, 3));
+		player.AddLink(Position(1, 1), Position(2, 3));
+		player.RemoveLink(Position(1, 1), Position(2, 3));
+		EXPECT_EQ(player.GetLinks().size(), 0);
 	}
 }
