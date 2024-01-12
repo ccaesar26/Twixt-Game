04ad7ea335830b9b02ca2c3253d0f7cfc944be4e
Aparascăi Andreea-Mădălina <aaparascai9@gmail.com>
2024-01-12 17:57:57 +0200

    Update IGame.h

--- TwixtLib/include/IGame.h
+++ TwixtLib/include/IGame.h
@@ -32,4 +32,5 @@ public:
 	virtual void LoadFromFile(const std::string& fileName) = 0;
 	virtual void SaveToFile(const std::string& fileName) const = 0;
 	virtual bool IsFileValid(const std::string& fileName) const = 0;
+	virtual void RequestDraw() = 0;
 };
